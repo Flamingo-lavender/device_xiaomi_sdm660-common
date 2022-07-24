305d6c57734c9a644b3ec9b89025934f83199902
xenxynon <flynryder427@gmail.com>
2022-07-24 05:58:37 +0000

    sdm660-common: switch to qti av component
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -312,10 +312,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libavservices_minijail.vendor
-
 # Network
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor \
@@ -369,7 +365,8 @@ PRODUCT_PACKAGES += \
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
-    perf
+    perf \
+    av
 
 # RIL
 PRODUCT_PACKAGES += \
